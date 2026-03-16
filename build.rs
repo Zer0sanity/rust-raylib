e3df1afdeffe0d602ac790c0a703b565d2c0bd07
zer0sanity <brian.rittner@gmail.com>
2026-03-16 05:29:07 -0500

    initial commit

--- build.rs
+++ build.rs
@@ -8,26 +8,34 @@ fn main() {
         .build();
 
     let bindings = bindgen::Builder::default()
-    .header("raylib/src/raylib.h")
-    .generate()
-    .expect("Unable to generate bindings");
+        .header("raylib/src/raylib.h")
+        .generate()
+        .expect("Unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings.write_to_file(out_path.join("bindings.rs")).expect("Couldn't write bindings!");
-    
+    bindings
+        .write_to_file(out_path.join("raylib_bindings.rs"))
+        .expect("Couldn't write bindings!");
+
     // Tell cargo where to find the library
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-search=native={}/lib64", dst.display());
     println!("cargo:rustc-link-lib=static=raylib");
 
     // Link system dependencies (vary by OS)
     #[cfg(target_os = "linux")]
     {
         println!("cargo:rustc-link-lib=GL");
+        println!("cargo:rustc-link-lib=X11");
+        println!("cargo:rustc-link-lib=Xcursor");
+        println!("cargo:rustc-link-lib=Xrandr");
+        println!("cargo:rustc-link-lib=Xinerama");
+        println!("cargo:rustc-link-lib=Xi");
         println!("cargo:rustc-link-lib=m");
         println!("cargo:rustc-link-lib=pthread");
         println!("cargo:rustc-link-lib=dl");
     }
 }
 // Local Variables:
-// jinx-local-words: "CMake dl pthread raylib rustc"
+// jinx-local-words: "CMake Xcursor Xinerama Xrandr dl pthread raylib rustc src"
 // End:
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use raylib::raylib;
+use raylib::Color;
+use raylib::Window;
+
+const SCREEN_WIDTH: i32 = 600;
+const SCREEN_HEIGHT: i32 = 600;
+const GRID_SIZE: i32 = 40;
+
 fn main() {
-    println!("Hello, world!");
+    let window = Window::new(SCREEN_WIDTH, SCREEN_HEIGHT, "hello world");
+
+    // <esc> will set this to true
+    while !window.should_close() {
+        raylib::begin_drawing();
+
+        raylib::clear_background(Color::BLACK);
+
+        raylib::draw_rectangle(0, 0, 50, 100, Color::RED);
+
+        raylib::end_drawing();
+    }
+
+    raylib::close_window();
 }
+
+// Local Variables:
+// jinx-local-words: "esc"
+// End:
